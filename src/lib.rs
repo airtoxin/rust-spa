0f0b2fe2ba388030635520dd45a203697b73abce
airtoxin <airtoxin@icloud.com>
2020-08-24 22:41:02 +0900

    Add counter sample

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,58 @@
-// src/lib.rs
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
-struct Hello {}
+struct Model {
+    link: ComponentLink<Self>,
+    value: i64,
+    minus_amount: i64
+}
 
-impl Component for Hello {
-    type Message = ();
+enum Msg {
+    AddOne,
+    ChangeMinusAmount { amount: String },
+    Minus
+}
+
+impl Component for Model {
+    type Message = Msg;
     type Properties = ();
-
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Self {}
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            value: 0,
+            minus_amount: 1
+        }
     }
 
-    fn update(&mut self, _: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => self.value += 1,
+            Msg::ChangeMinusAmount { amount } => self.minus_amount = amount.parse().unwrap(),
+            Msg::Minus => self.value -= self.minus_amount
+        }
         true
     }
 
-    fn change(&mut self, _: Self::Properties) -> ShouldRender {
-        true
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
     }
 
     fn view(&self) -> Html {
-        html! { <span>{"Hello World!"}</span> }
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                <input value=self.minus_amount type="number" oninput=self.link.callback(|e: InputData| Msg::ChangeMinusAmount { amount: e.value }) />
+                <button onclick=self.link.callback(|_| Msg::Minus)>{ format!("-{}", self.minus_amount) }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
     }
 }
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
-    App::<Hello>::new().mount_to_body();
+    App::<Model>::new().mount_to_body();
 }
